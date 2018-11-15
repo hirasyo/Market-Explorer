8525cd423c60914eef2212be57880dd069921c7d
平田祥一朗 <hirasho@hiratashouichirounoMacBook-ea.local>
2018-11-15 22:50:59 +0900

    error 403

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -20,7 +20,7 @@ class PagesController < ApplicationController
 
     if params["mercari"]
       # メルカリのURLからhtml情報を抽出
-      @html = search_html(url_mercari(params[:keyword]))
+      @html = search_html_mercari(url_mercari(params[:keyword]))
       # 抽出したhtmlをパース(解析)してオブジェクトを作成
       @result_mercari = Nokogiri::HTML.parse(@html, nil, 'utf-8')
       # 解析オブジェクトから必要な情報を抽出
@@ -131,10 +131,24 @@ class PagesController < ApplicationController
       return html,charset
     end
 
-    def search_html_rakuten(url)
+    def search_html_mercari(url)
+      opt = {}
+      opt['User-Agent'] = 'Ruby'
+      opt['Accept-Encoding'] = 'deflate'
+      opt['Accept-Language'] = 'ja,en-US;q=0.9,en;q=0.8'
       search_url = URI.encode url
-      html = open(search_url, "User-Agent"=>"Ruby")# htmlを読み込んで変数htmlに渡す
+      html = open(search_url, opt)# htmlを読み込んで変数htmlに渡す
       return html
+    end
+
+    def search_html_rakuten(url)
+      charset = nil
+      search_url = URI.encode url
+      html = open(search_url, "User-Agent"=>"Ruby") do |f|
+        charset = f.charset # 文字種別を取得
+        f.read # htmlを読み込んで変数htmlに渡す
+      end
+      return html,charset
     end
 
     def search_html_wowma(url)
